867d3776fb49e4f7ac4d4470ea05b74a900ec09e
Alyona Kovalyova <alyona+github@banka.space>
2020-07-14 16:55:19 +0300

    Add ticket entity

---
+++ src/main/java/ru/t_systems/alyona/sbb/entity/Ticket.java
@@ -0,0 +1,26 @@
+package ru.t_systems.alyona.sbb.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import java.math.BigInteger;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Ticket {
+
+    @Id
+    @GeneratedValue
+    private BigInteger id;
+
+    @ManyToOne
+    private User user;
+
+}
