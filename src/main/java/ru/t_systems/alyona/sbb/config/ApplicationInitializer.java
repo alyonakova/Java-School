9b2ceb94506f546fd15bd5ef8d84fb02e723c7fa
Alyona Kovalyova <alyona+github@banka.space>
2020-07-16 08:19:54 +0300

    Add application initializer

---
+++ src/main/java/ru/t_systems/alyona/sbb/config/ApplicationInitializer.java
@@ -0,0 +1,33 @@
+package ru.t_systems.alyona.sbb.config;
+
+import org.apache.commons.lang3.math.NumberUtils;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.request.RequestContextListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+public class ApplicationInitializer implements WebApplicationInitializer {
+
+    public void onStartup(ServletContext servletContext) throws ServletException {
+
+        AnnotationConfigWebApplicationContext annotationConfigWebApplicationContext = new AnnotationConfigWebApplicationContext();
+
+        annotationConfigWebApplicationContext.setServletContext(servletContext);
+        annotationConfigWebApplicationContext.scan("ru.t_systems.alyona.sbb.config");
+
+        servletContext.addListener(new ContextLoaderListener(annotationConfigWebApplicationContext));
+        servletContext.addListener(new RequestContextListener());
+
+        DispatcherServlet dispatcherServlet = new DispatcherServlet(annotationConfigWebApplicationContext);
+        dispatcherServlet.setThrowExceptionIfNoHandlerFound(true);
+        ServletRegistration.Dynamic dynamic = servletContext.addServlet("dispatcher", dispatcherServlet);
+        dynamic.addMapping("/");
+        dynamic.setLoadOnStartup(NumberUtils.INTEGER_ONE);
+    }
+
+}
