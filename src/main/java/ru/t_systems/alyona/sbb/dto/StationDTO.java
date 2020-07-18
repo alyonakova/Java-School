5d3e0b078dcca09bd51c35b31e02c554349c7e3e
Alyona Kovalyova <alyona+github@banka.space>
2020-07-18 18:25:47 +0300

    Create StationDTO

---
+++ src/main/java/ru/t_systems/alyona/sbb/dto/StationDTO.java
@@ -0,0 +1,27 @@
+package ru.t_systems.alyona.sbb.dto;
+
+import lombok.Data;
+
+import java.math.BigInteger;
+
+@Data
+public class StationDTO {
+
+    private BigInteger id;
+    private String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        StationDTO that = (StationDTO) o;
+        return id.equals(that.id) &&
+                name.equals(that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        return prime * (id.hashCode() + name.hashCode());
+    }
+}
