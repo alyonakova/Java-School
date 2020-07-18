c462fd8534b153ff311f2c0cbce38021355034dc
Alyona Kovalyova <alyona+github@banka.space>
2020-07-18 15:17:59 +0300

    Add PassengerDTO

---
+++ src/main/java/ru/t_systems/alyona/sbb/dto/PassengerDTO.java
@@ -0,0 +1,33 @@
+package ru.t_systems.alyona.sbb.dto;
+
+import lombok.Data;
+
+import java.math.BigInteger;
+import java.time.LocalDate;
+
+@Data
+public class PassengerDTO {
+
+    private BigInteger id;
+    private String name;
+    private String surname;
+    private LocalDate birthday;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PassengerDTO that = (PassengerDTO) o;
+        return id.equals(that.id) &&
+                name.equals(that.name) &&
+                surname.equals(that.surname) &&
+                birthday.equals(that.birthday);
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        return prime * (id.hashCode() + name.hashCode() + surname.hashCode() + birthday.hashCode());
+    }
+
+}
