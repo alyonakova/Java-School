4d7e7636dd9eae55b9cc5a4aa9bedfcb23123c63
Alyona Kovalyova <alyona+github@banka.space>
2020-07-20 07:49:22 +0300

    Delete unnecessary imports

--- src/main/java/ru/t_systems/alyona/sbb/repository/impl/SegmentRepositoryImpl.java
+++ src/main/java/ru/t_systems/alyona/sbb/repository/impl/SegmentRepositoryImpl.java
@@ -1,15 +1,10 @@
 package ru.t_systems.alyona.sbb.repository.impl;
 
-import ru.t_systems.alyona.sbb.entity.RoutePO;
 import ru.t_systems.alyona.sbb.entity.SegmentPO;
 import ru.t_systems.alyona.sbb.repository.SegmentRepository;
 
 import javax.persistence.EntityManager;
-import javax.persistence.TypedQuery;
 import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.List;
 
 public class SegmentRepositoryImpl implements SegmentRepository {
 
