c3190fb386a00b6ef8a401d8d6e79737bd7319ec
Alyona Kovalyova <alyona+github@banka.space>
2020-07-28 21:40:19 +0300

    Update security configuration, add user password to user details

--- src/main/java/ru/t_systems/alyona/sbb/config/SecurityConfig.java
+++ src/main/java/ru/t_systems/alyona/sbb/config/SecurityConfig.java
@@ -35,22 +35,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
 
-                .antMatchers("/customer_account").access("hasRole('CUSTOMER')")
-                .antMatchers("/employee_account", "/passengers", "/crud").access("hasRole('EMPLOYEE')")
+                .antMatchers("/customer_account").access("hasRole('ROLE_CUSTOMER')")
+                .antMatchers("/employee_account", "/passengers", "/crud").access("hasRole('ROLE_EMPLOYEE')")
                 .antMatchers("/", "/index", "/trains", "/ticket", "/sign_in").permitAll()
                 .and().
                 formLogin().loginPage("/sign_in").loginProcessingUrl("/perform_login")
                 .defaultSuccessUrl("/", true).
                 and().logout().logoutUrl("/perform_logout").logoutSuccessUrl("/sign_in?logout").permitAll()
                 .and().exceptionHandling().accessDeniedPage("/access_denied");
-    }
-
-    @Override
-    public void configure(AuthenticationManagerBuilder builder) throws Exception {
-        builder.inMemoryAuthentication()
-                .withUser("alyona")
-                .password("123")
-                .roles("EMPLOYEE");
     }
 
     @Bean
--- src/main/java/ru/t_systems/alyona/sbb/service/impl/CustomUserDetailsService.java
+++ src/main/java/ru/t_systems/alyona/sbb/service/impl/CustomUserDetailsService.java
@@ -1,6 +1,7 @@
 package ru.t_systems.alyona.sbb.service.impl;
 
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -27,14 +28,19 @@ public class CustomUserDetailsService implements UserDetailsService  {
         if (user==null) {
             throw new UsernameNotFoundException("Login not found");
         }
-        return new org.springframework.security.core.userdetails.User(user.getLogin(), "123", //TODO password
+        return new org.springframework.security.core.userdetails.User(user.getLogin(), user.getPassword(),
                 true, true, true, true, getGrantedAuthorities(user));
     }
 
     private List<GrantedAuthority> getGrantedAuthorities(UserDTO user){
         List<GrantedAuthority> authorities = new ArrayList<>();
-        String role = user.getIsPassenger() ? "CUSTOMER" : "EMPLOYEE";
+        String role = user.getIsPassenger() ? "ROLE_CUSTOMER" : "ROLE_EMPLOYEE";
         authorities.add(new SimpleGrantedAuthority(role));
         return authorities;
+    }
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
     }
 }
