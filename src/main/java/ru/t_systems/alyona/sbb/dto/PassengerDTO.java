664cab76d7a33fa23dcf1154e040886075ee4915
Alyona Kovalyova <alyona+github@banka.space>
2020-08-02 19:54:09 +0300

    Add validation to passenger DTO

--- src/main/java/ru/t_systems/alyona/sbb/dto/PassengerDTO.java
+++ src/main/java/ru/t_systems/alyona/sbb/dto/PassengerDTO.java
@@ -4,6 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
 import java.math.BigInteger;
 import java.time.LocalDate;
@@ -14,8 +18,16 @@ import java.time.LocalDate;
 public class PassengerDTO implements Serializable {
 
     private BigInteger id;
+    @NotNull(message = "Name can't be null")
+    @Size(min = 2, max = 30, message = "Min name length is 2, max is 30")
+    @Pattern(regexp = "^[a-zA-Z \\-]+$", message = "Latin letters, hyphens, spaces are allowed")
     private String name;
+    @NotNull(message = "Surname can't be null")
+    @Size(min = 2, max = 30, message = "Min surname length is 2, max is 30")
+    @Pattern(regexp = "^[a-zA-Z \\-]+$", message = "Latin letters, hyphens, spaces are allowed")
     private String surname;
+    @NotNull(message = "Birthday can't be null")
+    @Past(message = "Birthday must be past date")
     private LocalDate birthday;
 
 }
