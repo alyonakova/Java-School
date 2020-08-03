d181789d5c6e69e775f040a19738c6887ca025e5
Alyona Kovalyova <alyona+github@banka.space>
2020-08-03 08:47:25 +0300

    Add validation

--- src/main/java/ru/t_systems/alyona/sbb/dto/FindTrainFormDTO.java
+++ src/main/java/ru/t_systems/alyona/sbb/dto/FindTrainFormDTO.java
@@ -3,15 +3,20 @@ package ru.t_systems.alyona.sbb.dto;
 import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Data
 public class FindTrainFormDTO {
 
+    @NotNull(message = "First station name can't be null")
     String firstStationName;
+    @NotNull(message = "Last station name can't be null")
     String secondStationName;
     @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
+    @NotNull(message = "First date can't be null")
     LocalDateTime firstDate;
     @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
+    @NotNull(message = "Second date can't be null")
     LocalDateTime secondDate;
 }
