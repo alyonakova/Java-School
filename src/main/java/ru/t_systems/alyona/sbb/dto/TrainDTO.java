4da9a1cd2a2b2dec10056efad10690cb68597229
Alyona Kovalyova <alyona+github@banka.space>
2020-08-04 07:01:39 +0300

    Add validation to train dto

--- src/main/java/ru/t_systems/alyona/sbb/dto/TrainDTO.java
+++ src/main/java/ru/t_systems/alyona/sbb/dto/TrainDTO.java
@@ -2,10 +2,16 @@ package ru.t_systems.alyona.sbb.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
 @Data
 public class TrainDTO {
 
     private String id;
+
+    @Min(value = 5, message = "Min train capacity is 5")
+    @NotNull(message = "Train capacity can't be empty")
     private int seatsCount;
 
 }
