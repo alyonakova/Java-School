a0488024f7461d42b73ec112639d213e6d598b15
Alyona Kovalyova <alyona+github@banka.space>
2020-08-04 07:40:10 +0300

    Add validation to station dto

--- src/main/java/ru/t_systems/alyona/sbb/dto/StationDTO.java
+++ src/main/java/ru/t_systems/alyona/sbb/dto/StationDTO.java
@@ -2,6 +2,9 @@ package ru.t_systems.alyona.sbb.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.math.BigInteger;
 import java.time.ZoneId;
 
@@ -9,7 +12,13 @@ import java.time.ZoneId;
 public class StationDTO {
 
     private BigInteger id;
+
+    @NotNull(message = "Name can't be empty")
+    @Size(min = 3, max = 50, message = "Min name length is 3, max is 50")
+    @Pattern(regexp = "^[a-zA-Z \\-]+$", message = "Latin letters, hyphens, spaces are allowed")
     private String name;
+
+    @NotNull(message = "Zone id can't be empty")
     private ZoneId zoneId;
 
 }
