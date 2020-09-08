7472bee01b4d4e0324df54fbe02aba6f5342fb41
Alyona Kovalyova <alyona@banka.space>
2020-09-08 23:16:14 +0300

    Add tests for getting all stations

--- tickets-webapp/src/test/java/ru/t_systems/alyona/sbb/service/impl/StationServiceImplTest.java
+++ tickets-webapp/src/test/java/ru/t_systems/alyona/sbb/service/impl/StationServiceImplTest.java
@@ -3,6 +3,7 @@ package ru.t_systems.alyona.sbb.service.impl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.Nested;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -11,6 +12,8 @@ import ru.t_systems.alyona.sbb.dto.OperationResultDTO;
 import ru.t_systems.alyona.sbb.dto.StationDTO;
 import ru.t_systems.alyona.sbb.entity.StationEntity;
 import ru.t_systems.alyona.sbb.repository.StationRepository;
+
+import java.util.List;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.BDDMockito.given;
@@ -37,27 +40,49 @@ class StationServiceImplTest {
         stationEntity = mock(StationEntity.class);
     }
 
-    @Test
-    void returnsSuccessfulOperation_ifStationCreated() {
-        stationDTO = mock(StationDTO.class);
-        given(stationRepository.getByName(stationDTO.getName())).willReturn(null);
-        stationEntity = mock(StationEntity.class);
-        given(stationConverter.toEntity(stationDTO)).willReturn(stationEntity);
+    @Nested
+    class createStation {
 
-        OperationResultDTO result = stationService.createStation(stationDTO);
+        @Test
+        void returnsSuccessfulOperation_ifStationCreated() {
+            given(stationRepository.getByName(stationDTO.getName())).willReturn(null);
+            given(stationConverter.toEntity(stationDTO)).willReturn(stationEntity);
 
-        assertThat(result.isSuccessful()).isTrue();
+            OperationResultDTO result = stationService.createStation(stationDTO);
+
+            assertThat(result.isSuccessful()).isTrue();
+        }
+
+        @Test
+        void returnsErrorOperation_ifStationNameExists() {
+            given(stationRepository.getByName(stationDTO.getName())).willReturn(stationEntity);
+
+            OperationResultDTO result = stationService.createStation(stationDTO);
+
+            assertThat(result.isSuccessful()).isFalse();
+        }
     }
 
-    @Test
-    void returnsErrorOperation_ifStationNameExists() {
-        stationDTO = mock(StationDTO.class);
-        stationEntity = mock(StationEntity.class);
-        given(stationRepository.getByName(stationDTO.getName())).willReturn(stationEntity);
+    @Nested
+    class getAllStations {
 
-        OperationResultDTO result = stationService.createStation(stationDTO);
+        @Test
+        void returnsAllStations_ifTheyExist() {
+            given(stationRepository.getAll()).willReturn(List.of(stationEntity));
+            given(stationConverter.toDTOList(List.of(stationEntity))).willReturn(List.of(stationDTO));
 
-        assertThat(result.isSuccessful()).isFalse();
+            List<StationDTO> result = stationService.getAllStations();
+
+            assertThat(result).isNotEmpty();
+        }
+
+        @Test
+        void returnsEmptyList_ifNoStationsExist() {
+            given(stationRepository.getAll()).willReturn(List.of());
+
+            List<StationDTO> result = stationService.getAllStations();
+
+            assertThat(result).isEmpty();
+        }
     }
-
 }
