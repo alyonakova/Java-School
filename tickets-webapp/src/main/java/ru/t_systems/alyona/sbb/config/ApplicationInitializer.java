e76c2dcff799edec8bf63a609a248f30afb22cc4
Alyona Kovalyova <alyona@banka.space>
2020-09-13 21:56:27 +0300

    Split onStartup method into multiple methods for better readability

--- tickets-webapp/src/main/java/ru/t_systems/alyona/sbb/config/ApplicationInitializer.java
+++ tickets-webapp/src/main/java/ru/t_systems/alyona/sbb/config/ApplicationInitializer.java
@@ -1,32 +1,46 @@
 package ru.t_systems.alyona.sbb.config;
 
+import org.springframework.lang.NonNull;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.request.RequestContextListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 
 public class ApplicationInitializer implements WebApplicationInitializer {
 
-    public void onStartup(ServletContext servletContext) throws ServletException {
+    private static final String DISPATCHER_SERVLET_NAME = "dispatcher";
+    private static final String DISPATCHER_SERVLET_MAPPING = "/";
+    private static final String CONFIG_PACKAGE = ApplicationInitializer.class.getPackageName();
 
-        AnnotationConfigWebApplicationContext annotationConfigWebApplicationContext = new AnnotationConfigWebApplicationContext();
-
-        annotationConfigWebApplicationContext.setServletContext(servletContext);
-        annotationConfigWebApplicationContext.scan("ru.t_systems.alyona.sbb.config");
-
-        servletContext.addListener(new ContextLoaderListener(annotationConfigWebApplicationContext));
-        servletContext.addListener(new RequestContextListener());
-
-        DispatcherServlet dispatcherServlet = new DispatcherServlet(annotationConfigWebApplicationContext);
-        dispatcherServlet.setThrowExceptionIfNoHandlerFound(true);
-        ServletRegistration.Dynamic dynamic = servletContext.addServlet("dispatcher", dispatcherServlet);
-        dynamic.addMapping("/");
-        dynamic.setLoadOnStartup(1);
+    public void onStartup(@NonNull ServletContext servletContext) {
+        WebApplicationContext webAppContext = createWebApplicationContext(servletContext);
+        DispatcherServlet dispatcherServlet = createDispatcherServlet(webAppContext);
+        registerDispatcherServlet(servletContext, dispatcherServlet);
     }
 
+    private WebApplicationContext createWebApplicationContext(ServletContext servletContext) {
+        AnnotationConfigWebApplicationContext webApplicationContext = new AnnotationConfigWebApplicationContext();
+        webApplicationContext.setServletContext(servletContext);
+        webApplicationContext.scan(CONFIG_PACKAGE);
+        servletContext.addListener(new ContextLoaderListener(webApplicationContext));
+        servletContext.addListener(new RequestContextListener());
+        return webApplicationContext;
+    }
+
+    private DispatcherServlet createDispatcherServlet(WebApplicationContext webAppContext) {
+        DispatcherServlet dispatcherServlet = new DispatcherServlet(webAppContext);
+        dispatcherServlet.setThrowExceptionIfNoHandlerFound(true);
+        return dispatcherServlet;
+    }
+
+    private void registerDispatcherServlet(ServletContext servletContext, DispatcherServlet dispatcherServlet) {
+        ServletRegistration.Dynamic dynamic = servletContext.addServlet(DISPATCHER_SERVLET_NAME, dispatcherServlet);
+        dynamic.setLoadOnStartup(1);
+        dynamic.addMapping(DISPATCHER_SERVLET_MAPPING);
+    }
 }
